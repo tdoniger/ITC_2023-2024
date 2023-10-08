a972c8c681bdbf3815e8190f9b46273f888e6829
tdoniger <tirza@biomodel.os.biu.ac.il>
2023-10-08 17:46:47 +0300

    Create Program1.c

---
+++ Lesson1/Program1.c
@@ -0,0 +1,11 @@
+//This is a single line remark
+#include <iostream>
+using namespace std;
+/*This is a multiple
+line remark*/
+
+int main(){
+	cout<<"Bad News! Summer is over!!\n\n";
+  cout<<“Now we can learn \”C\”!!!\n”;
+  return 0;
+}
